b2fdc2be89e9cf2049791c6cd43ff66f568e03d6
Shin Asakawa <10644967+ShinAsakawa@users.noreply.github.com>
2022-02-14 21:40:17 +0900

    Add files via upload

--- mecab_settings.py
+++ mecab_settings.py
@@ -26,7 +26,9 @@ isColab = 'google.colab' in str(get_ipython())
 hostname = 'colab' if isColab else os.uname().nodename.split('.')[0]
 if isColab:
     wakati = MeCab.Tagger('-Owakati').parse
-    yomi = MeCab.Tagger('-Oyomi').parse
+    #yomi = MeCab.Tagger('-Oyomi').parse
+    yomi = MeCab.Tagger('-Oyomi -d /content/mecab-ipadic-neologd/build/mecab-ipadic-2.7.0-20070801-neologd-20200910').parse
+
 else:
     mecab_dic_dir = mecab_dic_dirs[hostname]
     wakati = MeCab.Tagger(f'-Owakati -d {mecab_dic_dir}').parse
